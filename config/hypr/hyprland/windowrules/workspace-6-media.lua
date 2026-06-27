1a9fc241641f8007cee63afb97253c5e3c629869
Alice <aliceneubauer@outlook.com>
2026-06-27 01:15:58 -0300

    fix: media and keybinds

--- config/hypr/hyprland/windowrules/workspace-6-media.lua
+++ config/hypr/hyprland/windowrules/workspace-6-media.lua
@@ -7,6 +7,6 @@ hl.config({ workspace = { "6, rounding:false, gapsin:0, gapsout:0, border:false"
 --  windowrule = workspace 6 silent, match:title ^(Stremio Web)$
 
 --  MPV
-hl.window_rule({ match = { class = "^(mpv)$" }, workspace = "6" })
-hl.window_rule({ match = { class = "^(mpv)$" }, immediate = true })
-hl.window_rule({ match = { class = "^(mpv)$" }, idle_inhibit = "focus" })
+-- hl.window_rule({ match = { class = "^(mpv)$" }, workspace = "6" })
+-- hl.window_rule({ match = { class = "^(mpv)$" }, immediate = true })
+-- hl.window_rule({ match = { class = "^(mpv)$" }, idle_inhibit = "focus" })
--- config/hypr/hyprland/windowrules/workspace-8-auxiliar.lua
+++ config/hypr/hyprland/windowrules/workspace-8-auxiliar.lua
@@ -3,3 +3,7 @@
 --  Aplicações que abrem silenciosamente no auxiliar
 hl.workspace_rule({ workspace = "8", monitor = "HDMI-A-1", no_rounding = true, decorate = false, gaps_in = 0, gaps_out = 0, no_border = true })
 hl.window_rule({ match = { class = "^(vesktop)$" }, workspace = "8 silent" })
+--  MPV
+hl.window_rule({ match = { class = "^(mpv)$" }, workspace = "6" })
+hl.window_rule({ match = { class = "^(mpv)$" }, immediate = true })
+hl.window_rule({ match = { class = "^(mpv)$" }, idle_inhibit = "focus" })
